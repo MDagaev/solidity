6523e20f3566cbb8fc3f74f4a626e88c5426bd9f
tavintavan@gmail.com <tavintavan@gmail.com>
2018-05-02 11:46:52 +0800

    Урок 8 Работа со структурами и функциями

--- Contarct.sol
+++ Contarct.sol
@@ -2,21 +2,22 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    // переменные
+    // У4 переменные
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     
-    // Структуры набор переменных Разных типов
+    // У5 Структуры набор переменных Разных типов
     struct Zombie {
         string name;
         uint dna;
     }
     
-    // Массивы набор переменных Одного типа, бываю Фиксированные [5] и Переменные [] 
+    // У6 Массивы набор переменных Одного типа, бываю Фиксированные [5] и Переменные [], privat, public 
     Zombie[] public zombies;
     
-    // Функции
+    // У7 Функции
     function createZombie (string _name, uint _dna){
+        zombies.push(Zombie(_name, _dna)); //У8 работа с струк-и и фун-и (Создаем нового Zombie(_name, _dna) и Добавляем push в массив zombies)
         
     }
 
