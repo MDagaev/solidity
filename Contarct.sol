58baed45b07b4199ebd32a4cbf52f73aca294581
tavintavan@gmail.com <tavintavan@gmail.com>
2018-05-02 11:22:14 +0800

    Урок 6 Массивы (набор перенных одного типа)

--- Contarct.sol
+++ Contarct.sol
@@ -2,12 +2,17 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    // переменные
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     
+    // Структуры набор переменных Разных типов
     struct Zombie {
         string name;
         uint dna;
     }
+    
+    // Массивы набор переменных Одного типа, бываю Фиксированные [5] и Переменные [] 
+    Zombie[] public zombies;
 
 }
